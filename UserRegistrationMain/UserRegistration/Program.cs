44073e093169010db5d278c1421703da62788c48
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-04-25 13:23:56 +0530

    ADDED: Validate first name

---
+++ UserRegistrationMain/UserRegistration/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static string NAME_PATTERN = "^[A-Z][a-z]{2,}$";
+        Regex regexName = new Regex(NAME_PATTERN);
+
+        public void patternCheck(string pattern)
+        {
+            if (regexName.IsMatch(pattern))
+            {
+                Console.WriteLine("Valid Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Name");
+            }
+        }
+
+        // Main Method
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the User registration Program!!!");
+            Program pg = new Program();
+            Console.WriteLine("Enter Your First Name : ");
+            string firstName = Console.ReadLine();
+            pg.patternCheck(firstName);
+        }
+    }
+}
