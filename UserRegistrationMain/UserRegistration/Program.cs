61b718f6e273d063ee9e10d606dcf92a4274c495
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-04-25 19:45:27 +0530

    ADDED: Validate email id

--- UserRegistrationMain/UserRegistration/Program.cs
+++ UserRegistrationMain/UserRegistration/Program.cs
@@ -6,7 +6,10 @@ namespace UserRegistration
     class Program
     {
         static string NAME_PATTERN = "^[A-Z][a-z]{2,}$";
+        static string EMAIL_PATTERN= "^([a-z0-9.-]+)@([a-z0-9-]+).([a-z]{2,8})(.[a-z]{2,8})?$";
+
         Regex regexName = new Regex(NAME_PATTERN);
+        Regex regEmail = new Regex(EMAIL_PATTERN);
 
         public void firstNameValidate(string firstName)
         {
@@ -19,19 +22,28 @@ namespace UserRegistration
                 Console.WriteLine("Invalid Name");
             }
         }
-            public void lastNameValidate(string lastName)
+        public void lastNameValidate(string lastName)
+        {
+            if (regexName.IsMatch(lastName))
             {
-                if (regexName.IsMatch(lastName))
-                {
-                    Console.WriteLine("Valid Pattern");
-                }
-                else
-                {
-                    Console.WriteLine("Invalid Pattern");
-                }
-
+                Console.WriteLine("Valid Pattern");
             }
-
+            else
+            {
+                Console.WriteLine("Invalid Pattern");
+            }
+        }
+        public void emailCheck(string emailPattern)
+        {
+            if (regEmail.IsMatch(emailPattern))
+            {
+                Console.WriteLine("Valid Pattern");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Pattern");
+            }
+        }
             static void Main(string[] args)
             {
                 Console.WriteLine("Welcome to the User registration Program!!!");
@@ -44,6 +56,10 @@ namespace UserRegistration
                 Console.WriteLine("Enter your last name : ");
                 string lastName = Console.ReadLine();
                 pg.lastNameValidate(lastName);
+
+                Console.WriteLine("Enter your Email id : ");
+                string emailID = Console.ReadLine();
+                pg.emailCheck(emailID);
             }
         }
     }
